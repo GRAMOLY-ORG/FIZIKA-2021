0a884dcf435ca74c22dbc266999d0049e69cd2eb
Aditya Singh <76763565+Adity-Singh@users.noreply.github.com>
2021-08-27 10:56:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fizika
+
+[Fizika](https://gramoly.org/c/fizika/) is a free annual competition meant for individuals at any stage that consists of the creative application of high school physics concepts. We aim to introduce learners to some unconventional applications of theory, hone up their problem-solving aptitude and expand their critical thinking, which in turn will make them better problem solvers. Furthermore, We love sharing some exciting problems with learners.
+
+**This repository is of fizika website. You can view it [here](https://gramoly.org/c/fizika/)**
---
+++ js/app.js
@@ -0,0 +1,110 @@
+particlesJS({
+  "particles": {
+    "number": {
+      "value": 147,
+      "density": {
+        "enable": true,
+        "value_area": 946.9771699587272
+      }
+    },
+    "color": {
+      "value": "#f02323"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 5,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 7
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 112.2388442605866,
+      "color": "#000000",
+      "opacity": 1,
+      "width": 0.9620472365193136
+    },
+    "move": {
+      "enable": true,
+      "speed": 4.810236182596568,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 7215.354273894853,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
